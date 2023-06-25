7fc52ce5d2343e964315fb02d14f16ec2bbc7a31
gerrardsarah <gerrardsarah@gmail.com>
2023-06-25 11:30:10 -0700

    head tag

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -1,8 +1,16 @@
+import Head from "next/head";
 import Header from "./Header";
 
 export default function LayOut({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Head>
+        <title>Sarah Gerrard | Clerk Application</title>
+        <meta
+          name="description"
+          content="A Clerk Application for Sarah Gerrard"
+        />
+      </Head>
       <Header />
       {children}
     </>
